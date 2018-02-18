51a14ea8bea049e985da6c4c13f0d4d34994fe48
Davis Cook <daviscook447@gmail.com>
2018-02-17 22:15:50 -0500

    Update Vacuum

--- src/main/java/fruitymod/cards/Vacuum.java
+++ src/main/java/fruitymod/cards/Vacuum.java
@@ -1,47 +1,34 @@
 package fruitymod.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
-import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.cards.status.Dazed;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
-import com.megacrit.cardcrawl.localization.LocalizedStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.monsters.MonsterGroup;
 import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.powers.WeakPower;
-import com.megacrit.cardcrawl.rooms.AbstractRoom;
-import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
-import com.megacrit.cardcrawl.vfx.combat.LightningEffect;
 import com.megacrit.cardcrawl.vfx.combat.VerticalImpactEffect;
 
 import basemod.abstracts.CustomCard;
 import fruitymod.FruityMod;
 import fruitymod.patches.AbstractCardEnum;
 
-import java.util.ArrayList;
-
 public class Vacuum
 extends CustomCard {
     public static final String ID = "Vacuum";
-    public static final String NAME = "Vacuum";
-    public static final String DESCRIPTION = "Remove all Weak, Frail, and Vulnerable. Deal !M! extra damage for each stack removed.";
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
+    private static final int UPGRADED_COST = 2;
     private static final int ATTACK_DMG = 0;
     private static final int ATTACK_UPGRADE = 0;
     private static final int STACK_BONUS = 4;
-    private static final int STACK_BONUS_UPGRADE = 3;
     private static final int POOL = 1;
 
     public Vacuum() {
@@ -102,7 +89,7 @@ extends CustomCard {
         if (!this.upgraded) {
             this.upgradeName();
             this.upgradeDamage(ATTACK_UPGRADE);
-            this.upgradeMagicNumber(STACK_BONUS_UPGRADE);
+            this.upgradeBaseCost(UPGRADED_COST);
         }
     }
 }
