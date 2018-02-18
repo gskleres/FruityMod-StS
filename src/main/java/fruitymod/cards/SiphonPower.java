606255f7603313268b69a6004b4adf38abe14ce7
Davis Cook <daviscook447@gmail.com>
2018-02-17 22:42:56 -0500

    Update and bug fix siphon power

--- src/main/java/fruitymod/cards/SiphonPower.java
+++ src/main/java/fruitymod/cards/SiphonPower.java
@@ -1,16 +1,16 @@
 package fruitymod.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 import com.megacrit.cardcrawl.powers.WeakPower;
 
@@ -20,14 +20,14 @@ import fruitymod.patches.AbstractCardEnum;
 
 public class SiphonPower extends CustomCard {
 	private static final String ID = "SiphonPower";
-	private static final String NAME = "Siphon Power";
-	private static final String DESCRIPTION = "If an enemy intends to attack, deal !D! damage, apply Weak, and gain !M! Strength.";
+	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+	public static final String NAME = cardStrings.NAME;
+	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 	private static final int COST = 2;
 	private static final int ATTACK_DMG = 10;
 	private static final int ATTACK_UPGRADE = 3;
-	private static final int STR_GAIN = 1;
-	private static final int STR_UPGRADE = 1;
-	private static final int WEAK_AMT = 1;
+	private static final int BUFF_DEBUFF_AMT = 1;
+	private static final int BUFF_DEBUFF_UPGRADE_AMT = 1;
 	private static final int POOL = 1;
 	
 	public SiphonPower() {
@@ -35,14 +35,14 @@ public class SiphonPower extends CustomCard {
 				AbstractCard.CardType.ATTACK, AbstractCardEnum.PURPLE,
 				AbstractCard.CardRarity.UNCOMMON, AbstractCard.CardTarget.ENEMY, POOL);
 		this.baseDamage = ATTACK_DMG;
-		this.magicNumber = this.baseMagicNumber = STR_GAIN;
+		this.magicNumber = this.baseMagicNumber = BUFF_DEBUFF_AMT;
 	}
 	
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
     	AbstractDungeon.actionManager.addToBottom(new DamageAction((AbstractCreature)m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new StrengthPower(p, this.magicNumber), this.magicNumber));
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, m, new WeakPower(m, WEAK_AMT, false), WEAK_AMT));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, p, new WeakPower(m, this.magicNumber, false), this.magicNumber));
     }
     
 	public boolean canUse(AbstractPlayer p, AbstractMonster m) {
@@ -66,7 +66,7 @@ public class SiphonPower extends CustomCard {
         if (!this.upgraded) {
             this.upgradeName();
             this.upgradeDamage(ATTACK_UPGRADE);
-            this.upgradeDamage(STR_UPGRADE);
+            this.upgradeDamage(BUFF_DEBUFF_UPGRADE_AMT);
         }
     }
 	
