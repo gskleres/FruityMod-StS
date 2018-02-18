7475dfefdc01e2dc1402038d92e392672624cfb6
Davis Cook <daviscook447@gmail.com>
2018-02-17 23:27:01 -0500

    Fix the description on Nebulous Blast for real

--- src/main/java/fruitymod/cards/NebulousBlast.java
+++ src/main/java/fruitymod/cards/NebulousBlast.java
@@ -21,7 +21,7 @@ extends CustomCard {
     public static final String ID = "NebulousBlast";
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
-    public static final String DESCRIPTION = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 2;
     private static final int ATTACK_DMG = 17;
     private static final int UPGRADE_PLUS_DMG = 5;
