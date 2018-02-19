cff76e7a3b73893c219f5518f7dc6db7444ab6ff
Davis Cook <daviscook447@gmail.com>
2018-02-19 15:42:33 -0500

    Fix typo in PeriaptOfPotency ID

--- src/main/java/fruitymod/cards/PeriaptOfPotency.java
+++ src/main/java/fruitymod/cards/PeriaptOfPotency.java
@@ -14,7 +14,7 @@ import fruitymod.patches.AbstractCardEnum;
 import fruitymod.powers.PotencyPower;
 
 public class PeriaptOfPotency extends CustomCard {
-	public static final String ID = "PeriaptOfPotency";
+	public static final String ID = "PeriaptofPotency";
 	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 	public static final String NAME = cardStrings.NAME;
 	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
