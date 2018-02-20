8d119dd4c71d50c1c93349aa49eda05b200e2baa
Davis Cook <daviscook447@gmail.com>
2018-02-20 02:50:18 -0500

    Update Flux Bolt

--- src/main/java/fruitymod/cards/FluxBolt.java
+++ src/main/java/fruitymod/cards/FluxBolt.java
@@ -23,8 +23,8 @@ extends CustomCard {
     public static final String NAME = "Flux Bolt";
     public static final String DESCRIPTION = "Deal !D! damage. Shuffle 1 Dazed into your Draw pile.";
     private static final int COST = 1;
-    private static final int ATTACK_DMG = 11;
-    private static final int UPGRADE_DMG_AMT = 4;
+    private static final int ATTACK_DMG = 10;
+    private static final int UPGRADE_DMG_AMT = 3;
     private static final int POOL = 1;
 
     public FluxBolt() {
