fa33be51bf4f17c5b05e2c9886b3d30bdc009262
Davis Cook <daviscook447@gmail.com>
2018-02-20 02:58:33 -0500

    Update Hypothesis

--- src/main/java/fruitymod/cards/Hypothesis.java
+++ src/main/java/fruitymod/cards/Hypothesis.java
@@ -1,19 +1,12 @@
 package fruitymod.cards;
 
-
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.EmptyDeckShuffleAction;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
-import com.megacrit.cardcrawl.localization.LocalizedStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import basemod.abstracts.CustomCard;
@@ -22,9 +15,10 @@ import fruitymod.patches.AbstractCardEnum;
 public class Hypothesis
 extends CustomCard {
     public static final String ID = "Hypothesis";
-    public static final String NAME = "Hypothesis";
-    public static final String DESCRIPTION = "Draw a card. If it is Ethereal, draw another card.";
-    public static final String UPGRADE_DESCRIPTION = "Draw a card. If it is Ethereal, draw !M! more cards.";
+	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+	public static final String NAME = cardStrings.NAME;
+	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+	public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     private static final int COST = 0;
     private static final int INITIAL_DRAW = 1;
     private static final int BONUS_DRAW = 1;
@@ -32,7 +26,9 @@ extends CustomCard {
     private static final int POOL = 1;
 
     public Hypothesis() {
-        super(ID, NAME, "images/cards/locked_skill.png", COST, DESCRIPTION, AbstractCard.CardType.SKILL, AbstractCardEnum.PURPLE, AbstractCard.CardRarity.COMMON, AbstractCard.CardTarget.SELF, POOL);
+        super(ID, NAME, "images/cards/locked_skill.png", COST, DESCRIPTION,
+        		AbstractCard.CardType.SKILL, AbstractCardEnum.PURPLE,
+        		AbstractCard.CardRarity.UNCOMMON, AbstractCard.CardTarget.SELF, POOL);
         this.baseMagicNumber = BONUS_DRAW;
     }
 
@@ -59,7 +55,7 @@ extends CustomCard {
         if (!this.upgraded) {
             this.upgradeName();
             this.upgradeMagicNumber(BONUS_DRAW_UPGRADE);
-            this.rawDescription = UPGRADE_DESCRIPTION + (this.isEthereal ? " NL Etherial." : "");
+            this.rawDescription = UPGRADE_DESCRIPTION;
             this.initializeDescription();
         }
     }
