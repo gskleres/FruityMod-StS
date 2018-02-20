422d445e29a58fa1ca88ccc0ee0ecf85ac5cf503
Davis Cook <daviscook447@gmail.com>
2018-02-20 14:22:10 -0500

    Make Coalescence add to bottom instead of top of action stack

--- src/main/java/fruitymod/powers/CoalescencePower.java
+++ src/main/java/fruitymod/powers/CoalescencePower.java
@@ -49,7 +49,7 @@ public class CoalescencePower extends AbstractPower {
 	@Override
 	public void atStartOfTurn() {
 		this.flash();
-		AbstractDungeon.actionManager.addToTop(
+		AbstractDungeon.actionManager.addToBottom(
 				new ApplyPowerAction(this.owner, this.owner,
 						new WeakPower(this.owner, this.weakAmount, false), this.weakAmount));
 	}
@@ -58,7 +58,7 @@ public class CoalescencePower extends AbstractPower {
 	public void atEndOfTurn(boolean isPlayer) {
 		if (isPlayer) {
 			this.flash();
-			AbstractDungeon.actionManager.addToTop(
+			AbstractDungeon.actionManager.addToBottom(
 					new GainBlockAction(this.owner, this.owner, this.amount));
 		}
 	}
