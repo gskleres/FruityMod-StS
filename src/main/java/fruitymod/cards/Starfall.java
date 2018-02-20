73fb04a28d968ce6b2dc959e1440855093516587
Davis Cook <daviscook447@gmail.com>
2018-02-20 02:53:30 -0500

    Update Starfall

--- src/main/java/fruitymod/cards/Starfall.java
+++ src/main/java/fruitymod/cards/Starfall.java
@@ -7,7 +7,9 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import basemod.abstracts.CustomCard;
@@ -16,8 +18,9 @@ import fruitymod.patches.AbstractCardEnum;
 
 public class Starfall extends CustomCard {
 	private static final String ID = "Starfall";
-	private static final String NAME = "Starfall";
-	private static final String DESCRIPTION = "Ethereal. Do !D! damage !M! times. Exhaust";
+	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+	public static final String NAME = cardStrings.NAME;
+	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 	private static final int COST = 1;
 	private static final int ATTACK_DMG = 3;
 	private static final int TIMES = 4;
