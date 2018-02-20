404016d92d3d3b03f06d962d92c7ec57d4c4e9db
Davis Cook <daviscook447@gmail.com>
2018-02-20 01:58:13 -0500

    Cleanup Zenith

--- src/main/java/fruitymod/cards/Zenith.java
+++ src/main/java/fruitymod/cards/Zenith.java
@@ -1,10 +1,7 @@
 package fruitymod.cards;
 
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.status.Dazed;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
