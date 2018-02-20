c04cb4bb2d22a92d67b4692812c8e4b2f10acb2a
Davis Cook <daviscook447@gmail.com>
2018-02-20 13:36:34 -0500

    Fix Hypothesis bug

--- src/main/java/fruitymod/actions/unique/HypothesisAction.java
+++ src/main/java/fruitymod/actions/unique/HypothesisAction.java
@@ -10,39 +10,54 @@ import basemod.BaseMod;
 import basemod.interfaces.PostDrawSubscriber;
 
 public class HypothesisAction extends AbstractGameAction implements PostDrawSubscriber {
-	private boolean firstDraw;
 	private boolean listening;
 	private int initialDraw;
 	
 	public HypothesisAction(AbstractCreature target, AbstractCreature source, int amount, int initialDraw) {
 		this.setValues(target, source, amount);
-		this.actionType = AbstractGameAction.ActionType.CARD_MANIPULATION;
-		this.duration = 0.5f;
+		this.actionType = AbstractGameAction.ActionType.WAIT;
 		this.initialDraw = initialDraw;
-		this.firstDraw = true;
 		BaseMod.subscribeToPostDraw(this);
 	}
 	
 	@Override
 	public void update() {
-		if (this.duration == 0.5f && this.firstDraw) {
-			this.firstDraw = false;
-			this.listening = true;
-			AbstractDungeon.actionManager.addToTop(new DrawCardAction(
-					AbstractDungeon.player, this.initialDraw));
-		} else if (!this.listening) {
-			this.isDone = true;
-		}
+		this.listening = true;
+		AbstractDungeon.actionManager.addToBottom(new DrawCardAction(
+				AbstractDungeon.player, this.initialDraw));
+		this.isDone = true;
 	}
 	
 	@Override
 	public void receivePostDraw(AbstractCard c) {
 		if (listening) {
+			this.listening = false;
 			if (c.isEthereal) {
-				this.listening = false;
-				AbstractDungeon.actionManager.addToTop(new DrawCardAction(
+				AbstractDungeon.actionManager.addToBottom(new DrawCardAction(
 						AbstractDungeon.player, this.amount));
 			}
+			/*
+			 *  calling unsubscribeFromPostBattle inside the callback
+			 *  for receivePostBattle means that when we're calling it
+			 *  there is currently an iterator going over the list
+			 *  of subscribers and calling receivePostBattle on each of
+			 *  them therefore if we immediately try to remove the this
+			 *  callback from the post battle subscriber list it will
+			 *  throw a concurrent modification exception in the iterator
+			 *  
+			 *  for now we just add a delay - yes this is an atrocious solution
+			 *  PLEASE someone with a better idea replace it
+			 */
+			Thread delayed = new Thread(() -> {
+				try {
+					Thread.sleep(200);
+				} catch (Exception e) {
+					System.out.println("could not delay unsubscribe to avoid ConcurrentModificationException");
+					e.printStackTrace();
+				}
+				BaseMod.unsubscribeFromPostDraw(this);
+			});
+			delayed.start();
 		}
 	}
 	
