f466bd51bfc3b927ee6d8902ddb673393819d2aa
Davis Cook <daviscook447@gmail.com>
2018-02-20 21:54:21 -0500

    Update Essence Dart

--- src/main/java/fruitymod/cards/EssenceDart.java
+++ src/main/java/fruitymod/cards/EssenceDart.java
@@ -1,10 +1,11 @@
 package fruitymod.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import basemod.abstracts.CustomCard;
@@ -13,16 +14,18 @@ import fruitymod.patches.AbstractCardEnum;
 
 public class EssenceDart extends CustomCard {
 	public static final String ID = "EssenceDart";
-	public static final String NAME = "Essence Dart";
-	public static final String DESCRIPTION = "Costs 1 less for each Ethereal card in hand. Deal !D! damage.";
+	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+	public static final String NAME = cardStrings.NAME;
+	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 	private static final int COST = 4;
 	private static final int ATTACK_DMG = 15;
 	private static final int UPGRADE_PLUS_DMG = 5;
 	private static final int POOL = 1;
 
 	public EssenceDart() {
-		super(ID, NAME, FruityMod.makePath(FruityMod.ESSENCE_DART), COST, DESCRIPTION, AbstractCard.CardType.ATTACK,
-				AbstractCardEnum.PURPLE, AbstractCard.CardRarity.UNCOMMON,
+		super(ID, NAME, FruityMod.makePath(FruityMod.ESSENCE_DART), COST, DESCRIPTION,
+				AbstractCard.CardType.ATTACK, AbstractCardEnum.PURPLE,
+				AbstractCard.CardRarity.COMMON,
 				AbstractCard.CardTarget.ENEMY, POOL);
 
 		this.baseDamage = ATTACK_DMG;
