b0bc07efab2ba760ca08f8f6260e64c78e72d101
Davis Cook <daviscook447@gmail.com>
2018-02-20 21:36:41 -0500

    Fix Magic Missile

--- src/main/java/fruitymod/cards/MagicMissile.java
+++ src/main/java/fruitymod/cards/MagicMissile.java
@@ -40,7 +40,6 @@ extends CustomCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(new VFXAction(new MindblastEffect(p.dialogX, p.dialogY)));
         AbstractDungeon.actionManager.addToBottom(new DamageAction((AbstractCreature)m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.NONE));
-        AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(p, p, new Dazed(), 1, true, true));
     }
 
     @Override
