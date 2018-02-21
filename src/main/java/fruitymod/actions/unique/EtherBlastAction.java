ab1a62d1117d70cea7886a6512fb615587a19ffa
Davis Cook <daviscook447@gmail.com>
2018-02-20 22:26:10 -0500

    Update Ether Blast

--- src/main/java/fruitymod/actions/unique/EtherBlastAction.java
+++ src/main/java/fruitymod/actions/unique/EtherBlastAction.java
@@ -1,20 +1,14 @@
 package fruitymod.actions.unique;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.helpers.Hitbox;
-import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
 import com.megacrit.cardcrawl.vfx.combat.ThrowDaggerEffect;
-import java.util.ArrayList;
 
 public class EtherBlastAction
 extends AbstractGameAction {
--- src/main/java/fruitymod/cards/EtherBlast.java
+++ src/main/java/fruitymod/cards/EtherBlast.java
@@ -1,5 +1,7 @@
 package fruitymod.cards;
 
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -7,18 +9,18 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.ThrowDaggerEffect;
 
 import basemod.abstracts.CustomCard;
 import fruitymod.FruityMod;
-import fruitymod.actions.unique.EtherBlastAction;
 import fruitymod.patches.AbstractCardEnum;
 
-// TODO: Flechettes adds additional information in play, shows how many attacks will be made, look into it to see if it should be added here.
 public class EtherBlast extends CustomCard {
 	public static final String ID = "EtherBlast";
 	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 	public static final String NAME = cardStrings.NAME;
 	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+	public static final String EXTENDED_DESCRIPTION = " NL (Deals !D! damage)";
 	private static final int COST = 1;
 	private static final int ATTACK_DMG = 5;
 	private static final int ATTACK_UPGRADE = 3;
@@ -27,13 +29,42 @@ public class EtherBlast extends CustomCard {
 	public EtherBlast() {
 		super(ID, NAME, FruityMod.makePath(FruityMod.ETHER_BLAST), COST, DESCRIPTION, AbstractCard.CardType.ATTACK,
 				AbstractCardEnum.PURPLE, AbstractCard.CardRarity.UNCOMMON, AbstractCard.CardTarget.ENEMY, POOL);
-		this.damage = this.baseDamage = ATTACK_DMG;
+		this.magicNumber = this.baseMagicNumber = ATTACK_DMG;
 	}
 
 	@Override
 	public void use(AbstractPlayer p, AbstractMonster m) {
-		AbstractDungeon.actionManager
-				.addToBottom(new EtherBlastAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn)));
+		for (AbstractCard c : AbstractDungeon.player.hand.group) {
+			if (c.isEthereal) {
+				AbstractDungeon.actionManager.addToTop(
+						new DamageAction(m,
+								new DamageInfo(p, this.damage, this.damageTypeForTurn), true));
+				AbstractDungeon.actionManager.addToTop(new VFXAction(new ThrowDaggerEffect(m.hb.cX, m.hb.cY)));
+			}
+		}
+		this.rawDescription = DESCRIPTION;
+		initializeDescription();
+	}
+	
+	@Override
+	public void applyPowers() {
+		int count  = 0;
+		for (AbstractCard c : AbstractDungeon.player.hand.group) {
+			if (c.isEthereal) {
+				count++;
+			}
+		}
+		this.baseDamage = count * this.magicNumber;
+		super.applyPowers();
+		this.rawDescription = DESCRIPTION + EXTENDED_DESCRIPTION;
+		initializeDescription();
+	}
+	
+	@Override
+	public void calculateCardDamage(AbstractMonster mo) {
+		super.calculateCardDamage(mo);
+		this.rawDescription = DESCRIPTION + EXTENDED_DESCRIPTION;
+		initializeDescription();
 	}
 
 	@Override
