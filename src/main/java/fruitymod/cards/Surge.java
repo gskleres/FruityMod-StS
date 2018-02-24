abedd8d87bf5b4a1f4752387d6badfe6d85548f6
Davis Cook <daviscook447@gmail.com>
2018-02-24 02:38:57 -0500

    Update Surge

--- src/main/java/fruitymod/cards/Surge.java
+++ src/main/java/fruitymod/cards/Surge.java
@@ -6,7 +6,9 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import basemod.abstracts.CustomCard;
@@ -15,10 +17,11 @@ import fruitymod.patches.AbstractCardEnum;
 
 public class Surge extends CustomCard {
 	public static final String ID = "Surge";
-	public static final String NAME = "Surge";
-	public static final String DESCRIPTION = "Deal !D! damage. NL If you are Frail or Vulnerable, deal !D! damage again.";
+	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+	public static final String NAME = cardStrings.NAME;
+	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 	private static final int COST = 1;
-	private static final int ATTACK_DMG = 7;
+	private static final int ATTACK_DMG = 5;
 	private static final int UPGRADE_DMG_AMT = 3;
 	private static final int POOL = 1;
 	
@@ -32,7 +35,7 @@ public class Surge extends CustomCard {
 	@Override
 	public void use(AbstractPlayer p, AbstractMonster m) {
 		AbstractDungeon.actionManager.addToBottom(new DamageAction((AbstractCreature)m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
-		if (AbstractDungeon.player.hasPower("Frail") || AbstractDungeon.player.hasPower("Vulnerable")) {
+		if (AbstractDungeon.player.hasPower("Frail") || AbstractDungeon.player.hasPower("Vulnerable") || AbstractDungeon.player.hasPower("Weakened")) {
 			AbstractDungeon.actionManager.addToBottom(new DamageAction((AbstractCreature)m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
 		}
 	}
