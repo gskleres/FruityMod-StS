4070c4362d32d5c4797615ba19726c39b17c6bb1
Davis Cook <daviscook447@gmail.com>
2018-02-24 00:57:42 -0500

    Fix Nova

--- src/main/java/fruitymod/cards/Nova.java
+++ src/main/java/fruitymod/cards/Nova.java
@@ -1,15 +1,14 @@
 package fruitymod.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.status.Dazed;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import basemod.abstracts.CustomCard;
@@ -18,8 +17,9 @@ import fruitymod.patches.AbstractCardEnum;
 
 public class Nova extends CustomCard {
     public static final String ID = "Nova";
-    public static final String NAME = "Nova";
-    public static final String DESCRIPTION = "Deal !D! damage to ALL enemies. NL Shuffle 2 Dazed into your draw pile.";
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+	public static final String NAME = cardStrings.NAME;
+	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 3;
     private static final int ATTACK_DMG = 20;
     private static final int UPGRADE_DMG_AMT = 10;
@@ -30,11 +30,12 @@ public class Nova extends CustomCard {
                 AbstractCard.CardType.ATTACK, AbstractCardEnum.PURPLE,
                 AbstractCard.CardRarity.RARE, CardTarget.ALL_ENEMY, POOL);
         this.baseDamage = ATTACK_DMG;
+        this.isMultiDamage = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(m, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.SLASH_HEAVY));
+        AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.SLASH_HEAVY));
         AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(p, p, new Dazed(), 2, true, true));
     }
 
