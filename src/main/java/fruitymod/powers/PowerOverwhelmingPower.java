c06ba2efb3cb0571e820b1ef56253634dd80c7bd
fiiiiilth <fiiiiilth@gmail.com>
2018-03-04 06:06:46 -0500

    Update Power Overwhelming

--- src/main/java/fruitymod/powers/PowerOverwhelmingPower.java
+++ src/main/java/fruitymod/powers/PowerOverwhelmingPower.java
@@ -3,7 +3,11 @@ package fruitymod.powers;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.status.Dazed;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.AbstractPower;
@@ -14,15 +18,10 @@ import fruitymod.FruityMod;
 public class PowerOverwhelmingPower extends AbstractPower {
 	public static final String POWER_ID = "PowerOverwhelmingPower";
 	public static final String NAME = "Power Overwhelming";
-	public static final String[] DESCRIPTIONS = new String[] {
-			"At the start of your turn gain ",
-			" Vulnerable. At the end of your turn deal ",
-			" damage to ALL enemies."
-	};
+	public static final String DESCRIPTION = "Whenever you play an attack, shuffle a Dazed into your draw pile.";
+
 	
-	private int vulnerableAmount;
-	
-	public PowerOverwhelmingPower(AbstractCreature owner, int vulnerableAmount, int amount) {
+	public PowerOverwhelmingPower(AbstractCreature owner, int amount) {
 		this.name = NAME;
 		this.ID = POWER_ID;
 		this.owner = owner;
@@ -30,39 +29,19 @@ public class PowerOverwhelmingPower extends AbstractPower {
 		this.type = AbstractPower.PowerType.BUFF;
 		this.isTurnBased = false;
 		this.priority = 90;
-		this.vulnerableAmount = vulnerableAmount;
 		updateDescription();
 		this.img = FruityMod.getPowerOverwhelmingPowerTexture();
+	}
+
+	@Override
+	public void onUseCard(AbstractCard card, UseCardAction action){
+		if(card.type == AbstractCard.CardType.ATTACK){
+			AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(AbstractDungeon.player, AbstractDungeon.player, new Dazed(), this.amount, true, true));
+		}
 	}
 	
 	@Override
 	public void updateDescription() {
-		this.description = DESCRIPTIONS[0] + this.vulnerableAmount +
-				DESCRIPTIONS[1] + this.amount + DESCRIPTIONS[2];
-	}
-	
-	@Override
-	public void stackPower(int stackAmount) {
-		this.fontScale = 8.0f;
-		this.amount += stackAmount;
-		this.vulnerableAmount += 1;
-	}
-	
-	@Override
-	public void atStartOfTurn() {
-		this.flash();
-		AbstractDungeon.actionManager.addToBottom(
-				new ApplyPowerAction(this.owner, this.owner,
-						new VulnerablePower(this.owner, this.vulnerableAmount, false), this.vulnerableAmount));
-	}
-	
-	@Override
-	public void atEndOfTurn(boolean isPlayer) {
-		if (isPlayer) {
-			this.flash();
-			AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(null, 
-					DamageInfo.createDamageMatrix(this.amount, true),
-					DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.FIRE));
-		}
+		this.description = DESCRIPTION;
 	}
 }
