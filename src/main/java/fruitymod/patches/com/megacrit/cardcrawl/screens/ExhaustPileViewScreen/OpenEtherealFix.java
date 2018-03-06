b86204287fc34934711dfda70630d2de46f75653
Davis Cook <daviscook447@gmail.com>
2018-03-05 23:34:07 -0500

    Fix ethereal in the exhaust pile view

---
+++ src/main/java/fruitymod/patches/com/megacrit/cardcrawl/screens/ExhaustPileViewScreen/OpenEtherealFix.java
@@ -0,0 +1,17 @@
+package fruitymod.patches.com.megacrit.cardcrawl.screens.ExhaustPileViewScreen;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+
+@SpirePatch(cls = "com.megacrit.cardcrawl.screens.ExhaustPileViewScreen", method = "open")
+public class OpenEtherealFix {
+	@SpireInsertPatch(rloc=17, localvars={"c", "toAdd"})
+	public static void Insert(Object __obj_instance, AbstractCard c, AbstractCard toAdd) {
+		if (!toAdd.isEthereal && c.isEthereal) {
+			toAdd.isEthereal = true;
+			toAdd.rawDescription = "Ethereal. " + toAdd.rawDescription;
+			toAdd.initializeDescription();
+		}
+	}
+}
