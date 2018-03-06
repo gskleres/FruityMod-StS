7171d892275f96e5c34ec7974211797c3ed5c18a
Davis Cook <daviscook447@gmail.com>
2018-03-05 23:03:41 -0500

    Get Entropy working

--- src/main/java/fruitymod/cards/Entropy.java
+++ src/main/java/fruitymod/cards/Entropy.java
@@ -1,18 +1,14 @@
 
 package fruitymod.cards;
 
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.powers.StrengthPower;
 
 import basemod.abstracts.CustomCard;
 import fruitymod.FruityMod;
@@ -20,52 +16,47 @@ import fruitymod.patches.AbstractCardEnum;
 
 public class Entropy extends CustomCard {
 	private static final String ID = "Entropy";
-	private static final String NAME = "Entropy";
-	private static final String DESCRIPTION = "Ethereal. NL Deal !D! damage. NL Enemy loses !M! Strength. NL Increase this cards values by 1 until the end of combat.";
+	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+	public static final String NAME = cardStrings.NAME;
+	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 	private static final int COST = 1;
-	private static final int ATTACK_DMG = 5;
-	private static final int ATTACK_UPGRADE = 3;
-	private static final int STR_LOSS = 1;
+	private static final int STR_LOSS = 2;
 	private static final int STR_LOSS_UPGRADE = 1;
 	private static final int POOL = 1;
-	
+
 	public Entropy() {
-		super (ID, NAME, FruityMod.makePath(FruityMod.ENTROPY), COST, DESCRIPTION,
-				AbstractCard.CardType.ATTACK, AbstractCardEnum.PURPLE,
-				AbstractCard.CardRarity.UNCOMMON, AbstractCard.CardTarget.ENEMY, POOL);
-		this.baseDamage = ATTACK_DMG;
+		super(ID, NAME, FruityMod.makePath(FruityMod.ENTROPY), COST, DESCRIPTION, AbstractCard.CardType.SKILL,
+				AbstractCardEnum.PURPLE, AbstractCard.CardRarity.UNCOMMON, AbstractCard.CardTarget.ENEMY, POOL);
 		this.magicNumber = this.baseMagicNumber = STR_LOSS;
 		this.isEthereal = true;
 	}
-	
-    @Override
-    public void use(AbstractPlayer p, AbstractMonster m) {
-    	AbstractDungeon.actionManager.addToBottom(new DamageAction((AbstractCreature)m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, p, new StrengthPower(m, - this.magicNumber), - this.magicNumber));
-        ++ this.baseDamage;
-        ++ this.baseMagicNumber;
-    }
-	
+
 	@Override
-    public void triggerOnEndOfPlayerTurn() {
-    	AbstractDungeon.actionManager.addToTop(new ExhaustSpecificCardAction(this, AbstractDungeon.player.hand));
-    }
-	
+	public void use(AbstractPlayer p, AbstractMonster m) {
+		AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, p,
+				new com.megacrit.cardcrawl.powers.StrengthPower(m, -1 * this.magicNumber), -1 * this.magicNumber));
+		AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, p,
+				new com.megacrit.cardcrawl.powers.LoseStrengthPower(m, -1 * this.magicNumber), -1 * this.magicNumber));
+		this.baseMagicNumber++;
+		this.magicNumber = this.baseMagicNumber;
+	}
+
 	@Override
-    public AbstractCard makeCopy() {
-        return new Entropy();
-    }
-    
-    @Override
-    public void upgrade() {
-        if (!this.upgraded) {
-            this.upgradeName();
-            this.upgradeDamage(ATTACK_UPGRADE);
-            this.upgradeMagicNumber(STR_LOSS_UPGRADE);
-        }
-    }
-	
+	public void triggerOnEndOfPlayerTurn() {
+		AbstractDungeon.actionManager.addToTop(new ExhaustSpecificCardAction(this, AbstractDungeon.player.hand));
+	}
+
+	@Override
+	public AbstractCard makeCopy() {
+		return new Entropy();
+	}
+
+	@Override
+	public void upgrade() {
+		if (!this.upgraded) {
+			this.upgradeName();
+			this.upgradeMagicNumber(STR_LOSS_UPGRADE);
+		}
+	}
+
 }
-
-
-
