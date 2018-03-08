38715cd45efdf4d73ed6c6391ff8ace05a14999b
George Skleres <george.skleres@gmail.com>
2018-03-07 20:36:53 -0500

    Brainstorm bug fix
    Upgraded version was drawing 3 cards instead of 2.

--- src/main/java/fruitymod/cards/Brainstorm.java
+++ src/main/java/fruitymod/cards/Brainstorm.java
@@ -22,7 +22,7 @@ public class Brainstorm extends CustomCard {
 	public static final String UPGRADE_DESCRIPTON = cardStrings.UPGRADE_DESCRIPTION;
     private static final int COST = 0;
     private static final int DRAW = 1;
-    private static final int DRAW_UPGRADE = 2;
+    private static final int DRAW_UPGRADE = 1;
     private static final int DAZED_COUNT = 1;
     private static final int POOL = 1;
     
