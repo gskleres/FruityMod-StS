ae3231982e79368363279c5e83c7b285de23648b
fiiiiilth <fiiiiilth@gmail.com>
2018-03-08 05:32:04 -0500

    Update Vortex

--- src/main/java/fruitymod/cards/Vortex.java
+++ src/main/java/fruitymod/cards/Vortex.java
@@ -1,7 +1,9 @@
 package fruitymod.cards;
 
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.status.Dazed;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -10,7 +12,6 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import basemod.abstracts.CustomCard;
 import fruitymod.FruityMod;
-import fruitymod.actions.unique.VortexAction;
 import fruitymod.patches.AbstractCardEnum;
 
 public class Vortex
@@ -20,23 +21,23 @@ extends CustomCard {
 	public static final String NAME = cardStrings.NAME;
 	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     private static final int COST = 1;
-    private static final int BLOCK = 12;
-    private static final int UPGRADE_BLOCK_AMT = 4;
-    private static final int DAZED_PER_CARD = 1;
+    private static final int UPGRADED_COST = 0;
     private static final int POOL = 1;
 
     public Vortex() {
         super(ID, NAME, FruityMod.makePath(FruityMod.VORTEX), COST, DESCRIPTION,
         		AbstractCard.CardType.SKILL, AbstractCardEnum.PURPLE,
         		AbstractCard.CardRarity.UNCOMMON, AbstractCard.CardTarget.SELF, POOL);
-        this.baseBlock = BLOCK;
         this.exhaust = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-    	AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, this.block));
-        AbstractDungeon.actionManager.addToBottom(new VortexAction(p, DAZED_PER_CARD));
+        int draw = 10 - AbstractDungeon.player.hand.size();
+        if(draw > 0) {
+            AbstractDungeon.actionManager.addToTop(new DrawCardAction(p, draw));
+            AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(p, p, new Dazed(), 4, false, false));
+        }
     }
 
     @Override
@@ -47,8 +48,8 @@ extends CustomCard {
     @Override
     public void upgrade() {
         if (!this.upgraded) {
-            this.upgradeName();	
-            this.upgradeBlock(UPGRADE_BLOCK_AMT);
+            this.upgradeName();
+            this.upgradeBaseCost(UPGRADED_COST);
             this.initializeDescription();
         }
     }
