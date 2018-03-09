1377c6cf4af31a7184be755b8539e92aed569de0
Davis Cook <daviscook447@gmail.com>
2018-03-08 20:27:06 -0500

    Proper Telescope implementation

---
+++ src/main/java/fruitymod/actions/unique/TelescopeAction.java
@@ -0,0 +1,92 @@
+package fruitymod.actions.unique;
+
+import java.util.ArrayList;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+public class TelescopeAction extends AbstractGameAction {
+
+	private static final String SELECT_TEXT = "Select any number of cards to make Ethereal";
+	
+	private AbstractPlayer p;
+	private ArrayList<AbstractCard> cannotEtherealize = new ArrayList<>();
+	
+	public TelescopeAction() {	
+		this.p = AbstractDungeon.player;
+		this.duration = Settings.ACTION_DUR_MED;
+		this.actionType = AbstractGameAction.ActionType.CARD_MANIPULATION;
+	}
+	
+	public void update() {
+		if (this.duration == Settings.ACTION_DUR_MED) {
+			System.out.println("looking for cards in hand");
+			for (AbstractCard c : this.p.hand.group) {
+				System.out.println("card " + c.name + " found in hand");
+				if (c.isEthereal) {
+					this.cannotEtherealize.add(c);
+				}
+			}
+			
+			for (AbstractCard c : this.cannotEtherealize) {
+				System.out.println("card " + c.name + " added to pile of cards that cannot be made ethereal");
+			}
+			
+			if (this.cannotEtherealize.size() == this.p.hand.group.size()) {
+				System.out.println("all cards are already ethereal");
+				this.isDone = true;
+				return;
+			}
+			
+			System.out.println("removing ethereal cards from the hand");
+			this.p.hand.group.removeAll(this.cannotEtherealize);
+			
+			for (AbstractCard c : this.p.hand.group) {
+				System.out.println("card " + c.name + " left in hand after removing ethereal cards");
+			}
+			
+			System.out.println("opening card select for " + this.p.hand.size() + " cards");
+			AbstractDungeon.handCardSelectScreen.open(
+					SELECT_TEXT, 99, true, true);
+			AbstractDungeon.actionManager.addToBottom(new com.megacrit.cardcrawl.actions.utility.WaitAction(0.25F));
+			tickDuration();
+			return;
+		}
+		
+		if (!AbstractDungeon.handCardSelectScreen.wereCardsRetrieved) {
+			System.out.println("cards were retrieved!");
+			for (AbstractCard c : AbstractDungeon.handCardSelectScreen.selectedCards.group) {
+				c.isEthereal = true;
+				c.rawDescription = "Ethereal. NL " + c.rawDescription;
+				c.initializeDescription();
+				System.out.println("adding the card " + c.name + " back to the player's hand as an ethereal card");
+				this.p.hand.addToTop(c);
+			}
+			
+			System.out.println("returning ethereal cards to hand");
+			returnCards();
+			AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true;
+			AbstractDungeon.handCardSelectScreen.selectedCards.group.clear();
+			this.isDone = true;
+		}
+
+		tickDuration();
+	}
+	
+	private void returnCards() {
+		for (AbstractCard c : this.cannotEtherealize) {
+			this.p.hand.addToTop(c);
+		}
+		
+		// apply powers
+		for (AbstractCard c : this.p.hand.group) {
+			c.applyPowers();
+		}
+		
+		this.p.hand.refreshHandLayout();
+	}
+	
+}
--- src/main/java/fruitymod/relics/Telescope.java
+++ src/main/java/fruitymod/relics/Telescope.java
@@ -1,22 +1,15 @@
 package fruitymod.relics;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.powers.ArtifactPower;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 
 import basemod.abstracts.CustomRelic;
 import fruitymod.FruityMod;
-import fruitymod.actions.common.DiscardWithCallbackAction;
-import fruitymod.actions.common.IDiscardCallback;
+import fruitymod.actions.unique.TelescopeAction;
 
 public class Telescope extends CustomRelic {
 	private static final String ID = "Telescope";
-	private static final int DISCARD_AMT = 1;
-	private static final int ARTIFACT_PER_DISCARD = 1;
 
 	public Telescope() {
 		super(ID, FruityMod.getTelescopeTexture(),
@@ -33,15 +26,7 @@ public class Telescope extends CustomRelic {
 		AbstractDungeon.actionManager.addToBottom(
 				new RelicAboveCreatureAction(AbstractDungeon.player, this));
 		this.flash();
-		AbstractPlayer p = AbstractDungeon.player;
-		AbstractDungeon.actionManager.addToBottom(new DiscardWithCallbackAction(
-				p, p, DISCARD_AMT, false, true, false, false, new IDiscardCallback() {
-					@Override
-					public void processCard(AbstractCard c) {
-						AbstractDungeon.actionManager.addToTop(
-								new ApplyPowerAction(p, p, new ArtifactPower(p, ARTIFACT_PER_DISCARD), ARTIFACT_PER_DISCARD));
-					}
-				}));
+		AbstractDungeon.actionManager.addToBottom(new TelescopeAction());
 	}
 
 	@Override
