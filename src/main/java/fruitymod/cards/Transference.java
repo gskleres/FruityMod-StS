3fae54a8de10bf0298b9c64fcbeb4850fcbeb379
gskleres <george.skleres@gmail.com>
2018-03-31 02:46:35 -0400

    Transference
    Now also draws a card in both versions.
    Transference can now be played even if you don't have any Vulnerable,
    Frail, or Weak.

--- src/main/java/fruitymod/cards/Transference.java
+++ src/main/java/fruitymod/cards/Transference.java
@@ -1,6 +1,7 @@
 package fruitymod.cards;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
@@ -17,6 +18,7 @@ import com.megacrit.cardcrawl.powers.VulnerablePower;
 
 import basemod.abstracts.CustomCard;
 import fruitymod.FruityMod;
+import fruitymod.actions.unique.HypothesisAction;
 import fruitymod.patches.AbstractCardEnum;
 
 public class Transference extends CustomCard {
@@ -26,13 +28,16 @@ public class Transference extends CustomCard {
 	    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 	    private static final int COST = 1;
 	    private static final int COST_UPGRADED = 0;
+	    private static final int DRAW = 1;
 	    private static final int POOL = 1;
 	    
 	 public Transference() {
 		 super(ID, NAME,  FruityMod.makePath(FruityMod.TRANSFERENCE), COST, DESCRIPTION,
 				 AbstractCard.CardType.SKILL, AbstractCardEnum.PURPLE,
 				 AbstractCard.CardRarity.UNCOMMON, AbstractCard.CardTarget.ENEMY, POOL);
-	    }
+		 this.baseMagicNumber = DRAW;
+		 this.magicNumber = DRAW;
+		 }
 	 
 	    @Override
 	    public void use(AbstractPlayer p, AbstractMonster m) {
@@ -46,15 +51,7 @@ public class Transference extends CustomCard {
 	    		AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(p, p, "Vulnerable"));
 	    		AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, p, new VulnerablePower(m, powerCount, false), powerCount));
 	    	}
-	    }
-	    
-	    @Override
-	    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
-	    	if(GetPowerCount(p, "Weakened") + GetPowerCount(p, "Vulnerable") > 0) {
-	    		return true;
-	    	}
-	    	this.cantUseMessage = cardStrings.EXTENDED_DESCRIPTION[0];
-	        return false;
+	    	AbstractDungeon.actionManager.addToBottom(new DrawCardAction(p, this.magicNumber));
 	    }
 	    
 	    private int GetPowerCount(AbstractCreature c, String powerId) {
