fafb7ba3af836fe65c097f94599a96fb9f6bb2d7
gskleres <george.skleres@gmail.com>
2018-04-06 01:28:56 -0400

    Phase Coil
    updated for #33

--- src/main/java/fruitymod/cards/PhaseCoil.java
+++ src/main/java/fruitymod/cards/PhaseCoil.java
@@ -1,6 +1,8 @@
 package fruitymod.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.ModifyDamageAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -17,10 +19,10 @@ public class PhaseCoil extends CustomCard {
 	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 	public static final String NAME = cardStrings.NAME;
 	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-	private static final int COST = 4;
-	private static final int UPGRADE_COST = 3;
-	private static final int ATTACK_DMG = 15;
-	private static final int UPGRADE_PLUS_DMG = 3;
+	private static final int COST = 2;
+	private static final int ATTACK_DMG = 9;
+	private static final int ATTACK_DMG_UPGRADE = 6;
+	private static final int DMG_BONUS_WHEN_DRAWN = 3;
 	private static final int POOL = 1;
 
 	public PhaseCoil() {
@@ -29,35 +31,18 @@ public class PhaseCoil extends CustomCard {
 				AbstractCard.CardRarity.COMMON,
 				AbstractCard.CardTarget.ENEMY, POOL);
 
-		this.baseDamage = ATTACK_DMG;
+		this.damage = this.baseDamage = ATTACK_DMG;
+		this.magicNumber = this.baseMagicNumber = DMG_BONUS_WHEN_DRAWN;
 	}
 	
-    @Override
-    public void triggerWhenDrawn() {
+	@Override
+	public void triggerWhenDrawn(){
         super.triggerWhenDrawn();
-        this.setCostForTurn(this.cost - getEtherealCount());
+		AbstractDungeon.actionManager.addToBottom(new ModifyDamageAction(this, this.magicNumber));
     }
     
-    @Override
-    public void applyPowers() {
-    	super.applyPowers();
-    	this.setCostForTurn(this.cost - getEtherealCount());
-    }
-    
-    private int getEtherealCount() {
-    	int count = 0;
-        for (AbstractCard c : AbstractDungeon.player.hand.group) {
-            if (!c.isEthereal) continue;
-            ++count;
-        }
-        return count;
-    }
-
-
 	public void use(com.megacrit.cardcrawl.characters.AbstractPlayer p, AbstractMonster m) {
-			AbstractDungeon.actionManager.addToBottom(new com.megacrit.cardcrawl.actions.common.DamageAction(m,
-					new DamageInfo(p, this.damage, this.damageTypeForTurn),
-					AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+			AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
 	}
 
 	public AbstractCard makeCopy() {
@@ -67,8 +52,7 @@ public class PhaseCoil extends CustomCard {
 	public void upgrade() {
 		if (!this.upgraded) {
 			upgradeName();
-			upgradeBaseCost(UPGRADE_COST);
-			upgradeDamage(UPGRADE_PLUS_DMG);
+			upgradeDamage(ATTACK_DMG_UPGRADE);
 		}
 	}
 }
