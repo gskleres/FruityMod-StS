413b7a759c833dbab3065ffd4f890a1f29cc9185
gskleres <george.skleres@gmail.com>
2018-04-06 17:20:00 -0400

    Comet
    updated #17

--- src/main/java/fruitymod/cards/Comet.java
+++ src/main/java/fruitymod/cards/Comet.java
@@ -3,6 +3,7 @@ package fruitymod.cards;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -17,59 +18,58 @@ import basemod.abstracts.CustomCard;
 import fruitymod.FruityMod;
 import fruitymod.patches.AbstractCardEnum;
 
-public class Comet
-extends CustomCard {
+public class Comet extends CustomCard {
     public static final String ID = "Comet";
 	private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 	public static final String NAME = cardStrings.NAME;
 	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+	public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
     private static final int COST = 1;
-    private static final int ATTACK_DMG = 5;
-    private static final int UPGRADE_DMG_AMT = 3;
-    private static final int ETHERAL_DMG = 1;
-    private static final int UPGRADE_ETHEREAL_DMG_AMT = 1;
+    private static final int PER_CARD_DAMAGE = 2;
+    private static final int PER_CARD_DAMAGE_UPGRADED = 1;
     private static final int POOL = 1;
 
     public Comet() {
         super(ID, NAME, FruityMod.makePath(FruityMod.COMET), COST, DESCRIPTION,
         		AbstractCard.CardType.ATTACK, AbstractCardEnum.PURPLE,
-        		AbstractCard.CardRarity.UNCOMMON, AbstractCard.CardTarget.ENEMY, POOL);
-        this.baseDamage = ATTACK_DMG;
-        this.magicNumber = this.baseMagicNumber = ETHERAL_DMG;
+        		AbstractCard.CardRarity.COMMON, AbstractCard.CardTarget.ENEMY, POOL);
+        this.magicNumber = this.baseMagicNumber = PER_CARD_DAMAGE;
     }
     
-    public static int countCards() {
-    	int count = 0;
-    	for (AbstractCard c : AbstractDungeon.player.hand.group) {
-    		if (c.isEthereal) {
-    			count++;
-    		}
-    	}
-    	for (AbstractCard c : AbstractDungeon.player.drawPile.group) {
-    		if (c.isEthereal) {
-    			count++;
-    		}
-    	}
-    	for (AbstractCard c : AbstractDungeon.player.discardPile.group) {
-    		if (c.isEthereal) {
-    			count++;
-    		}
-    	}
-    	return count;
-    }
-    
-    @Override
-    public float calculateModifiedCardDamage(AbstractPlayer player, AbstractMonster mo, float tmp) {
-    	return tmp + this.magicNumber * countCards();
-    }
-    
-    @Override
-    public void use(AbstractPlayer p, AbstractMonster m) {
+	@Override
+	public void use(AbstractPlayer p, AbstractMonster m) {
         if (m != null) {
         	AbstractDungeon.actionManager.addToBottom(new VFXAction(new WeightyImpactEffect(m.hb.cX, m.hb.cY)));
         }
+        AbstractDungeon.actionManager.addToBottom(new WaitAction(0.8f));
         AbstractDungeon.actionManager.addToBottom(new DamageAction((AbstractCreature)m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.NONE));
-    }
+	}
+	
+	@Override
+	public void onMoveToDiscard() {
+		this.setDescription(false);
+	}
+	
+	@Override
+	public void applyPowers() {
+		this.damage = this.baseDamage = (AbstractDungeon.player.hand.size() - 1) * this.magicNumber;
+		super.applyPowers();
+		this.setDescription(true);
+	}
+	
+	@Override
+	public void calculateCardDamage(AbstractMonster mo) {
+		super.calculateCardDamage(mo);
+		this.setDescription(true);
+	}
+	
+	private void setDescription(boolean addExtended) {
+		this.rawDescription = DESCRIPTION;
+		if (addExtended) {
+			this.rawDescription += EXTENDED_DESCRIPTION[0];
+		}
+		this.initializeDescription();
+	}
 
     @Override
     public AbstractCard makeCopy() {
@@ -80,8 +80,7 @@ extends CustomCard {
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeDamage(UPGRADE_DMG_AMT);
-            this.upgradeMagicNumber(UPGRADE_ETHEREAL_DMG_AMT);
+            this.upgradeMagicNumber(PER_CARD_DAMAGE_UPGRADED);
         }
     }
 }
