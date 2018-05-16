a059708373666a75ff72fc427d1e8047b610ea2b
fiiiiilth <fiiiiilth@gmail.com>
2018-05-16 15:50:20 -0400

    Fixed Reflect keyword
    #187

--- src/main/java/fruitymod/FruityMod.java
+++ src/main/java/fruitymod/FruityMod.java
@@ -662,7 +662,7 @@ public class FruityMod implements PostInitializeSubscriber,
 	@Override
 	public void receiveEditKeywords() {
         logger.info("setting up custom keywords");
-        BaseMod.addKeyword(new String[] {"reflect", "Reflect"}, "Whenever you are attacked this turn, deal this amount of damage back back to the attacker.");
+        BaseMod.addKeyword(new String[] {"reflect", "Reflect"}, "Whenever you are attacked this turn, deal this amount of damage back to ALL enemies.");
         BaseMod.addKeyword(new String[] {"top-cycle", "Top-Cycle"}, "When you Top-Cycle a card, place it on top of your draw pile.");
         BaseMod.addKeyword(new String[] {"recycle", "Recycle"}, "When you Recycle a card, shuffle it randomly into your draw pile.");
 	}
