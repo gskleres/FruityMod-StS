08d1caf3310236a23aa752eceb23f91f6d5c227a
fiiiiilth <fiiiiilth@gmail.com>
2018-07-12 15:46:13 -0400

    Umbra rework
    Umbra (1 Energy, Uncommon Attack): Deal 4(6) damage. Gain 3(5) Block. If you are Weak, Frail, or Vulnerable, repeat these effects.

--- src/main/java/fruitymod/cards/Umbra.java
+++ src/main/java/fruitymod/cards/Umbra.java
@@ -23,47 +23,48 @@ import fruitymod.FruityMod;
 import fruitymod.patches.AbstractCardEnum;
 
 public class Umbra extends CustomCard {
-	 	public static final String ID = "Umbra";
-	    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
-		public static final String NAME = cardStrings.NAME;
-		public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-	    private static final int COST = 1;
-	    private static final int ATTACK_DMG = 8;
-	    private static final int ATTACK_UPGRADE = 4;
-	    private static final int BLOCK = 8;
-	    private static final int BLOCK_UPGRADE = 4;
+    public static final String ID = "Umbra";
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    private static final int COST = 1;
+    private static final int ATTACK_DMG = 4;
+    private static final int ATTACK_UPGRADE = 2;
+    private static final int BLOCK = 3;
+    private static final int BLOCK_UPGRADE = 2;
 
-	 public Umbra() {
-		 super(ID, NAME, FruityMod.makePath(FruityMod.UMBRAL_BOLT), COST, DESCRIPTION, AbstractCard.CardType.SKILL,
-				 AbstractCardEnum.SEEKER_PURPLE, AbstractCard.CardRarity.UNCOMMON, CardTarget.SELF);
-	        this.damage = this.baseDamage = ATTACK_DMG;
-	        this.block = this.baseBlock = BLOCK;
-	        this.isMultiDamage = true;
-	    }
-	 
-	    @Override
-	    public void use(AbstractPlayer p, AbstractMonster m) {
-			if (AbstractDungeon.player.hasPower("Frail") || AbstractDungeon.player.hasPower("Vulnerable")) {
-				AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, this.block));
-			}
-			if (AbstractDungeon.player.hasPower("Weakened")) {
-		        AbstractDungeon.actionManager.addToBottom(new SFXAction("ATTACK_HEAVY"));
-		        AbstractDungeon.actionManager.addToBottom(new VFXAction(p, new CleaveEffect(), 0.1f));
-		        AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.NONE));
-			}
-	    }
+    public Umbra() {
+        super(ID, NAME, FruityMod.makePath(FruityMod.UMBRAL_BOLT), COST, DESCRIPTION, CardType.ATTACK,
+                AbstractCardEnum.SEEKER_PURPLE, AbstractCard.CardRarity.UNCOMMON, CardTarget.ENEMY);
+        this.damage = this.baseDamage = ATTACK_DMG;
+        this.block = this.baseBlock = BLOCK;
+    }
 
-	    @Override
-	    public AbstractCard makeCopy() {
-	        return new Umbra();
-	    }
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (p.hasPower("Frail") || p.hasPower("Vulnerable") || p.hasPower("Weakened")) {
+            AbstractDungeon.actionManager.addToBottom(new SFXAction("ATTACK_HEAVY"));
+            AbstractDungeon.actionManager.addToBottom(new VFXAction(p, new CleaveEffect(), 0.1f));
+            AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn)));
+            AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, this.block));
+        }
+        AbstractDungeon.actionManager.addToBottom(new SFXAction("ATTACK_HEAVY"));
+        AbstractDungeon.actionManager.addToBottom(new VFXAction(p, new CleaveEffect(), 0.1f));
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn)));
+        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, this.block));
+    }
 
-	    @Override
-	    public void upgrade() {
-	        if (!this.upgraded) {
-	            this.upgradeName();
-	            this.upgradeDamage(ATTACK_UPGRADE);
-	            this.upgradeBlock(BLOCK_UPGRADE);
-	        }
-	    }
+    @Override
+    public AbstractCard makeCopy() {
+        return new Umbra();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeDamage(ATTACK_UPGRADE);
+            this.upgradeBlock(BLOCK_UPGRADE);
+        }
+    }
 }
