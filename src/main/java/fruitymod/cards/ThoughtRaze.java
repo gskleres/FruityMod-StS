0198c8ae70a91b4ef3a1c01db71471b06a6fca63
fiiiiilth <fiiiiilth@gmail.com>
2018-07-12 17:13:03 -0400

    Thought Raze now shows how many Ethereal cards are in the Draw pile
    fixes #177

--- src/main/java/fruitymod/cards/ThoughtRaze.java
+++ src/main/java/fruitymod/cards/ThoughtRaze.java
@@ -22,6 +22,7 @@ extends CustomCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
     private static final int COST = 2;
     private static final int ATTACK_DMG = 7;
     private static final int UPGRADE_PLUS_DMG = 3;
@@ -31,6 +32,7 @@ extends CustomCard {
         		AbstractCard.CardType.ATTACK, AbstractCardEnum.SEEKER_PURPLE,
         		AbstractCard.CardRarity.RARE, AbstractCard.CardTarget.ENEMY);
         this.baseDamage = ATTACK_DMG;
+        this.magicNumber = this.baseMagicNumber = 0;
     }
 
     @Override
@@ -48,6 +50,39 @@ extends CustomCard {
             		new DamageAction((AbstractCreature) m, 
             				new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
     	}
+    }
+
+    public static int countEtherealInDrawPile() {
+        int etherealCount = 0;
+        for (AbstractCard c : AbstractDungeon.player.drawPile.group) {
+            if (!c.isEthereal)
+                continue;
+            etherealCount ++;
+        }
+        return etherealCount;
+    }
+
+    @Override
+    public void applyPowers() {
+        this.magicNumber = countEtherealInDrawPile();
+        super.applyPowers();
+        setDescription(true);
+    }
+
+    private void setDescription(boolean addExtended) {
+        this.rawDescription = (this.isEthereal ? "Ethereal. " : "") + DESCRIPTION;
+        if (addExtended && this.magicNumber == 1) {
+            this.rawDescription += EXTENDED_DESCRIPTION[0] + this.magicNumber + EXTENDED_DESCRIPTION[1] + EXTENDED_DESCRIPTION[2] + EXTENDED_DESCRIPTION[4];
+        }
+        else {
+            this.rawDescription += EXTENDED_DESCRIPTION[0] + this.magicNumber + EXTENDED_DESCRIPTION[1] + EXTENDED_DESCRIPTION[3] + EXTENDED_DESCRIPTION[4];
+        }
+        this.initializeDescription();
+    }
+
+    @Override
+    public void onMoveToDiscard() {
+        this.setDescription(false);
     }
 
     @Override
