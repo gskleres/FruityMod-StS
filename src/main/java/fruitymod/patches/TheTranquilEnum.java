041b6a9d695a47674e968117825b49281514942b
boommies <boommies@gmail.com>
2018-07-28 02:06:00 -0400

    Initial sketch at creating new Tranquil character

---
+++ src/main/java/fruitymod/patches/TheTranquilEnum.java
@@ -0,0 +1,12 @@
+package fruitymod.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+
+public class TheTranquilEnum {
+
+	@SpireEnum
+	public static AbstractPlayer.PlayerClass THE_TRANQUIL;
+	
+	
+}
