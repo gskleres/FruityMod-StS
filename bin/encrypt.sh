544f498e47963113e999a9266cb02053f2ad5311
boommies <boommies@gmail.com>
2018-08-10 17:17:55 -0400

    Only source dependency_overrides.properties if it exists

--- bin/encrypt.sh
+++ bin/encrypt.sh
@@ -1,4 +1,6 @@
-source ./dependency_overrides.properties
+if [ -f dependency_overrides.properties ]; then
+  source dependency_overrides.properties
+fi
 
 echo "Encrypting $1 and saving as $1.gpg"
 gpg --symmetric --batch --no-symkey-cache -c --cipher-algo AES256 --passphrase $GPG_PASSPHRASE $1
