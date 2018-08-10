bab52280adc4ec75e7e56ededd0c073e63430331
boommies <boommies@gmail.com>
2018-08-09 22:50:05 -0400

    Always download the latest BaseMod.jar (provide optional override)

--- bin/download_dependencies.sh
+++ bin/download_dependencies.sh
@@ -1,6 +1,7 @@
 MOD_THE_SPIRE_LATEST_RELEASE_URL=https://github.com/kiooeht/ModTheSpire/releases/latest
 #MOD_THE_SPIRE_ZIP_URL=https://github.com/kiooeht/ModTheSpire/releases/download/v2.9.1/ModTheSpire.zip
-BASE_MOD_JAR_URL=https://github.com/daviscook477/BaseMod/releases/download/v2.18.0/BaseMod.jar
+BASE_MOD_LATEST_RELEASE_URL=https://github.com/daviscook477/BaseMod/releases/latest
+#BASE_MOD_JAR_URL=https://github.com/daviscook477/BaseMod/releases/download/v2.18.0/BaseMod.jar
 DESKTOP_JAR_LOCAL_PATH="$HOME/Library/Application Support/Steam/steamapps/common/SlayTheSpire/SlayTheSpire.app/Contents/Resources/desktop-1.0.jar"
 
 if [ -f dependency_overrides.properties ]; then
@@ -45,9 +46,18 @@ makeModDirectory() {
 getBaseModJar() {
   if [ -f ./mods/BaseMod.jar ]; then
     echo "./mods/BaseMod.jar already present - skipping."
-  else
-    echo "Downloading BaseMod.jar"
+    return
+  fi
+
+  if [ -z $BASE_MOD_JAR_URL ]; then
+    BASE_MOD_JAR_URL=`curl $BASE_MOD_LATEST_RELEASE_URL 2> /dev/null | sed -E "s/.*href=\"([^\"]*)\".*/\1\/BaseMod.jar/g; s/tag/download/g"`
+  fi
+
+  if [ ! -z $BASE_MOD_JAR_URL ]; then
+    echo "Downloading BaseMod.jar from $BASE_MOD_JAR_URL"
     curl -L $BASE_MOD_JAR_URL > ./mods/BaseMod.jar
+  else
+    echo "Could not download BaseMod.jar - try downloading it manually from $BASE_MOD_LATEST_RELEASE_URL"
   fi
 }
 
