c5d49cb8067e06d2a60588af7c4c058123c6ca6c
boommies <boommies@gmail.com>
2018-08-12 17:30:43 -0400

    Remove unnecessary whitespace change

--- src/main/java/fruitymod/FruityMod.java
+++ src/main/java/fruitymod/FruityMod.java
@@ -510,7 +510,6 @@ public class FruityMod implements PostInitializeSubscriber,
 		BaseMod.addCard(new Dazed_P());
 		BaseMod.addCard(new Strike_Purple());
 		BaseMod.addCard(new Defend_Purple());
-
 		BaseMod.addCard(new Starburst());
 		BaseMod.addCard(new Irradiate());
 		BaseMod.addCard(new AstralHaze());
