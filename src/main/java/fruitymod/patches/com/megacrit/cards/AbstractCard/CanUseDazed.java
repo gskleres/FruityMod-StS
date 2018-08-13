742985f3c34bbfe3175f78994a586e37a1767af6
boommies <boommies@gmail.com>
2018-08-13 18:33:24 -0400

    Move remaining Enigma power to SeekerMod

--- src/main/java/fruitymod/patches/com/megacrit/cards/AbstractCard/CanUseDazed.java
+++ src/main/java/fruitymod/patches/com/megacrit/cards/AbstractCard/CanUseDazed.java
@@ -13,7 +13,7 @@ public class CanUseDazed {
 			public void edit(MethodCall m) throws CannotCompileException {
 				if (m.getMethodName().equals("hasRelic")) {
 					// pass the original argument (relicID) + this ($0 which is the AbstractCard)
-					m.replace("{ $_ = fruitymod.FruityMod.hasRelicCustom($1, this); }");
+					m.replace("{ $_ = fruitymod.SeekerMod.hasRelicCustom($1, this); }");
 				}
 			}
 		};
