dca850a0d53bc540fcfbd337184deefadda4b357
boommies <boommies@gmail.com>
2018-08-14 09:48:05 -0400

    Add enw TranquilMod

---
+++ src/main/java/fruitymod/TranquilMod.java
@@ -0,0 +1,61 @@
+package fruitymod;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+
+public class TranquilMod implements CharacterMod {
+	@Override
+	public void receiveEditCards() {
+
+	}
+
+	@Override
+	public void receiveEditCharacters() {
+
+	}
+
+	@Override
+	public void receiveEditKeywords() {
+
+	}
+
+	@Override
+	public void receiveEditRelics() {
+
+	}
+
+	@Override
+	public void receiveEditStrings() {
+
+	}
+
+	@Override
+	public void receiveCardUsed(AbstractCard abstractCard) {
+
+	}
+
+	@Override
+	public void receivePowersModified() {
+
+	}
+
+	@Override
+	public void receivePostBattle(AbstractRoom abstractRoom) {
+
+	}
+
+	@Override
+	public void receivePostDraw(AbstractCard abstractCard) {
+
+	}
+
+	@Override
+	public void receivePostDungeonInitialize() {
+
+	}
+
+	@Override
+	public void receivePostExhaust(AbstractCard abstractCard) {
+
+	}
+}
