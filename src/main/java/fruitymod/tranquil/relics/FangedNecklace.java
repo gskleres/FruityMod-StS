5e9a1ec5b7fc41e539470f8557688fe8e5af475c
boommies <boommies@gmail.com>
2018-09-04 13:14:13 -0400

    FangedNecklace adds 1 energy each turn

--- src/main/java/fruitymod/tranquil/relics/FangedNecklace.java
+++ src/main/java/fruitymod/tranquil/relics/FangedNecklace.java
@@ -2,6 +2,9 @@ package fruitymod.tranquil.relics;
 
 import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import fruitymod.SeekerMod;
 import fruitymod.TranquilMod;
@@ -24,6 +27,13 @@ public class FangedNecklace extends CustomRelic {
                 DESCRIPTIONS[1] +
                 BLOCK_PER_ATTACK +
                 DESCRIPTIONS[2];
+    }
+
+    @Override
+    public void atTurnStart() {
+        AbstractDungeon.actionManager.addToBottom(
+                new GainEnergyAction(ENERGY_PER_TURN));
+        this.flash();
     }
 
     @Override
