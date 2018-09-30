9c6c95f0bb696a960d74d9e71f57e1c83d458868
SilentJ28 <sanjeev28@gmail.com>
2018-09-29 22:54:35 -0700

    Remove dependency on deprecated DailyMods

--- src/main/java/fruitymod/tranquil/characters/TheTranquil.java
+++ src/main/java/fruitymod/tranquil/characters/TheTranquil.java
@@ -5,7 +5,6 @@ import basemod.animations.SpriterAnimation;
 import com.megacrit.cardcrawl.actions.utility.ExhaustAllEtherealAction;
 import com.megacrit.cardcrawl.core.EnergyManager;
 import com.megacrit.cardcrawl.core.Settings;
-import com.megacrit.cardcrawl.daily.DailyMods;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.screens.CharSelectInfo;
@@ -40,10 +39,6 @@ public class TheTranquil extends CustomPlayer {
 				TranquilMod.makeCharacterImagePath("shoulder"),
 				TranquilMod.makeCharacterImagePath("corpse"),
 				getLoadout(), 20.0F, -10.0F, 220.0F, 290.0F, new EnergyManager(ENERGY_PER_TURN));
-
-		if (Settings.dailyModsEnabled() && DailyMods.cardMods.get("Diverse")) {
-			this.masterMaxOrbs = 1;
-		}
 	}
 	
 	@Override
