999e9a51ca99192ba186858c6e9c1cf5e3908b4e
Vex Darkon <edanvirgo@gmail.com>
2020-01-26 19:57:12 -0700

    Retrograde up!

--- src/main/java/fruitymod/seeker/cards/Retrograde.java
+++ src/main/java/fruitymod/seeker/cards/Retrograde.java
@@ -1,9 +1,7 @@
 package fruitymod.seeker.cards;
 
-import com.badlogic.gdx.graphics.Color;
+import basemod.abstracts.CustomCard;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
@@ -13,50 +11,53 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.vfx.combat.VerticalAuraEffect;
-
-import basemod.abstracts.CustomCard;
 import fruitymod.SeekerMod;
 import fruitymod.seeker.patches.AbstractCardEnum;
-import fruitymod.seeker.powers.RetrogradePower;
-import fruitymod.seeker.powers.RetrogradeUpgradedPower;
 
 public class Retrograde extends CustomCard {
-	public static final String ID = "Retrograde";
+    public static final String ID = "Retrograde";
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-	private static final int COST = 0;
-	private static final int ATTACK_DMG = 4;
+    private static final int COST = 5;
+    private static final int ATTACK_DMG = 9;
 
-	public Retrograde() {
-		super(ID, NAME, SeekerMod.makeCardImagePath(ID) , COST, DESCRIPTION,
-				AbstractCard.CardType.ATTACK,
-				AbstractCardEnum.SEEKER_PURPLE, AbstractCard.CardRarity.COMMON,
-				AbstractCard.CardTarget.ENEMY);
-		this.damage = this.baseDamage = ATTACK_DMG;
-	}
+    public Retrograde() {
+        super(ID, NAME, SeekerMod.makeCardImagePath(ID), COST, DESCRIPTION,
+                AbstractCard.CardType.ATTACK,
+                AbstractCardEnum.SEEKER_PURPLE, AbstractCard.CardRarity.COMMON,
+                AbstractCard.CardTarget.ENEMY);
+        this.damage = this.baseDamage = ATTACK_DMG;
+    }
 
-	@Override
-	public void use(AbstractPlayer p, AbstractMonster m) {
-		AbstractDungeon.actionManager.addToBottom(new DamageAction((AbstractCreature) m,
-				new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
-		AbstractDungeon.actionManager
-				.addToBottom(new VFXAction(p, new VerticalAuraEffect(Color.FIREBRICK, p.hb.cX, p.hb.cY), 0.0f));
-		AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, 
-				(this.upgraded ? new RetrogradeUpgradedPower(p, 1) : new RetrogradePower(p, 1)), 1));
-	}
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new DamageAction((AbstractCreature) m,
+                new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        AbstractDungeon.actionManager.addToBottom(new DamageAction((AbstractCreature) m,
+                new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+    }
 
-	@Override
-	public AbstractCard makeCopy() {
-		return new Retrograde();
-	}
+    @Override
+    public void triggerWhenDrawn() {
+        updateCost(-1);
+        if (this.canUse(AbstractDungeon.player, null)) {
+            beginGlowing();
+        } else {
+            stopGlowing();
+        }
+    }
 
-	@Override
-	public void upgrade() {
-		if (!this.upgraded) {
-			this.upgradeName();
-			this.upgradeDamage(2);
-		}
-	}
+    @Override
+    public AbstractCard makeCopy() {
+        return new Retrograde();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeBaseCost(4);
+        }
+    }
 }
