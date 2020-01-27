45b1f8e0fe18d9b4bf0505d5d6bddab1136041f2
Vex Darkon <edanvirgo@gmail.com>
2020-01-26 19:38:44 -0700

    New Cosmic Sieve

--- src/main/java/fruitymod/seeker/relics/CosmicSieve.java
+++ src/main/java/fruitymod/seeker/relics/CosmicSieve.java
@@ -1,29 +1,39 @@
 package fruitymod.seeker.relics;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.megacrit.cardcrawl.relics.AbstractRelic;
-
 import basemod.abstracts.CustomRelic;
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
 import fruitymod.SeekerMod;
 
 
 public class CosmicSieve extends CustomRelic {
-	public static final String ID = "CosmicSieve";
-	public static final int HP_PER_CARD = 1;
-	
-	public CosmicSieve() {
-		super(ID, new Texture(SeekerMod.makeRelicImagePath(ID)),
-				RelicTier.BOSS, LandingSound.MAGICAL);
-	}
-	
-	@Override
-	public String getUpdatedDescription() {
-		return DESCRIPTIONS[0] + HP_PER_CARD + DESCRIPTIONS[1];
-	}
-	
-	@Override
-	public AbstractRelic makeCopy() {
-		return new CosmicSieve();
-	}
-	
+    public static final String ID = "CosmicSieve";
+
+    public CosmicSieve() {
+        super(ID, new Texture(SeekerMod.makeRelicImagePath(ID)),
+                RelicTier.BOSS, LandingSound.MAGICAL);
+    }
+
+    @Override
+    public void onEnterRestRoom() {
+        flash();
+        int i = 0;
+        for (AbstractCard c : AbstractDungeon.player.masterDeck.group) {
+            if (c.isEthereal) i++;
+        }
+        if (i > 0) AbstractDungeon.player.heal(i, true);
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+    @Override
+    public AbstractRelic makeCopy() {
+        return new CosmicSieve();
+    }
+
 }
