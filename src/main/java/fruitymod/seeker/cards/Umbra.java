4bf94e88d1c59fc71e072e2116354b2e24bc2aa6
Vex Darkon <edanvirgo@gmail.com>
2020-01-26 21:32:56 -0700

    Umbra!

--- src/main/java/fruitymod/seeker/cards/Umbra.java
+++ src/main/java/fruitymod/seeker/cards/Umbra.java
@@ -2,18 +2,18 @@ package fruitymod.seeker.cards;
 
 
 import basemod.abstracts.CustomCard;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.vfx.combat.CleaveEffect;
+import com.megacrit.cardcrawl.powers.FrailPower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
 import fruitymod.SeekerMod;
 import fruitymod.seeker.patches.AbstractCardEnum;
 
@@ -22,31 +22,27 @@ public class Umbra extends CustomCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    private static final int COST = 1;
-    private static final int ATTACK_DMG = 4;
-    private static final int ATTACK_UPGRADE = 2;
-    private static final int BLOCK = 3;
-    private static final int BLOCK_UPGRADE = 2;
+    private static final int COST = 0;
+    private static final int ATTACK_DMG = 12;
+    private static final int ATTACK_UPGRADE = 4;
+    private static final int BLOCK = 12;
+    private static final int BLOCK_UPGRADE = 4;
 
     public Umbra() {
         super(ID, NAME, SeekerMod.makeCardImagePath(ID), COST, DESCRIPTION, CardType.ATTACK,
-                AbstractCardEnum.SEEKER_PURPLE, AbstractCard.CardRarity.UNCOMMON, CardTarget.ENEMY);
+                AbstractCardEnum.SEEKER_PURPLE, CardRarity.COMMON, CardTarget.ENEMY);
         this.damage = this.baseDamage = ATTACK_DMG;
         this.block = this.baseBlock = BLOCK;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        if (p.hasPower("Frail") || p.hasPower("Vulnerable") || p.hasPower("Weakened")) {
-            AbstractDungeon.actionManager.addToBottom(new SFXAction("ATTACK_HEAVY"));
-            AbstractDungeon.actionManager.addToBottom(new VFXAction(p, new CleaveEffect(), 0.1f));
-            AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn)));
-            AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, this.block));
+        if (p.hasPower(VulnerablePower.POWER_ID) || p.hasPower(FrailPower.POWER_ID)) {
+            addToBot(new GainBlockAction(p, block));
         }
-        AbstractDungeon.actionManager.addToBottom(new SFXAction("ATTACK_HEAVY"));
-        AbstractDungeon.actionManager.addToBottom(new VFXAction(p, new CleaveEffect(), 0.1f));
-        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn)));
-        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, this.block));
+        if (p.hasPower(WeakPower.POWER_ID)) {
+            addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+        }
     }
 
     @Override
